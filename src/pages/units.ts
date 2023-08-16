c76e1017007d8167ee8e68e182f7962a5f94d349
Ernest Trachuk <etrachuk@web3tech.ru>
2023-08-16 14:14:04 +0200

    added wrapers

--- src/pages/units.ts
+++ src/pages/units.ts
@@ -14,3 +14,32 @@ export const PageWrapper = styled.div`
 
     overflow: hidden;
 `;
+
+export const ColumnWrapper = styled.div<{ gap?: number; fullWidth?: boolean }>`
+    display: flex;
+    flex-direction: column;
+    gap: ${({ gap = 8 }) => `${gap}px`};
+
+    width: ${({ fullWidth }) => fullWidth && '100%'};
+`;
+
+export const FlexAlignCenterWrapper = styled.div<{
+    gap?: number;
+    fullWidth?: boolean;
+    isCenter?: boolean;
+}>`
+    display: flex;
+    align-items: ${({ isCenter = true }) => isCenter && 'center'};
+    gap: ${({ gap = 8 }) => `${gap}px`};
+
+    width: ${({ fullWidth }) => fullWidth && '100%'};
+`;
+
+export const FlexBetweenWrapper = styled.div<{ gap?: number; fullWidth?: boolean }>`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: ${({ gap = 8 }) => `${gap}px`};
+
+    width: ${({ fullWidth }) => fullWidth && '100%'};
+`;
